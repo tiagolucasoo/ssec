cd4741fd97334a5474317de7f0c3556e0506f37c
Tiago Lucas <tiago.lucas.oliveira18@gmail.com>
2025-05-20 18:18:55 -0300

    Aplicando Interface Cadastro

--- model/cadastro_model.py
+++ model/cadastro_model.py
@@ -2,9 +2,42 @@
 
 import sqlite3
 
-def cadastro_categoria_M():
-    conn = sqlite3.connect("banco.db")
+#Cria o Banco de Dados de Cadastro de Produtos
+def BD_cad_produtos():
+    conn = sqlite3.connect("../banco.db")
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO categorias VALUES (?, ?, ?)", (codigo, descricao))
-    conn.commit()
-    conn.close()
+    print("Banco conectado!")
+
+    #cursor.execute("INSERT INTO categorias VALUES (?, ?, ?)", (codigo, descricao))
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS cad_produtos(
+            codigo_produto INTEGER PRIMARY KEY AUTOINCREMENT,
+            codigobarras INT(30),
+            descricao_produto VARCHAR(50) NOT NULL,
+            marca VARCHAR(20) NOT NULL,
+            categoria VARCHAR(40) NOT NULL,
+            custo DECIMAL(8,2) NOT NULL,
+            venda DECIMAL(8,2) NOT NULL
+        )
+    ''')
+    print("Tabela cad_produtos já existe ou foi criada!\n")
+    
+#Cria o Banco de Dados de Cadastro de Categorias
+def BD_cad_categorias():
+    conn = sqlite3.connect("../banco.db")
+    cursor = conn.cursor()
+    print("Banco conectado!")
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS cad_categorias(
+            codigo_categoria INTEGER PRIMARY KEY AUTOINCREMENT,
+            descricao_produto VARCHAR(50) NOT NULL
+        )          
+    ''')
+    print("Tabela cad_categorias já existe ou foi criada!\n")
+
+    conn.commit() #Confirma as alterações
+    conn.close() #Encerra o BD
+
+#Chamada de Criação do Banco de Cadastro
+chamadaBD = BD_cad_categorias(), BD_cad_produtos()
--- view/cadastro_produtos_view.py
+++ view/cadastro_produtos_view.py
@@ -12,6 +12,35 @@ class App(customtkinter.CTk):
         self.geometry("400x500")
         self.title("SSEC - Cadastro de Produtos")
 
+        #Código do Produto
+        label = customtkinter.CTkLabel(self, text="Código", fg_color="transparent")
+        label.pack()
+        entry = customtkinter.CTkEntry(self, placeholder_text="Exemplo: 1010")
+        entry.pack()
+
+        #Código de Barras
+        label2 = customtkinter.CTkLabel(self, text="Código de Barras", fg_color="transparent")
+        label2.pack()
+        entry2 = customtkinter.CTkEntry(self, placeholder_text="Exemplo: 7896051020127")
+        entry2.pack()
+
+        #Descrição do Produto
+        label3 = customtkinter.CTkLabel(self, text="Descrição do Produto", fg_color="transparent")
+        label3.pack()
+        entry3 = customtkinter.CTkEntry(self, placeholder_text="Exemplo: Energético Furioso 300ml Tradicional")
+        entry3.pack()
+
+        #Marca
+        label4 = customtkinter.CTkLabel(self, text="Marca", fg_color="transparent")
+        label4.pack()
+        entry4 = customtkinter.CTkEntry(self, placeholder_text="Value")
+        entry4.pack()
+
+        radiobutton_1 = customtkinter.CTkRadioButton(self, text="CTkRadioButton 1", command="", value=1)
+        radiobutton_1.pack()
+        radiobutton_2 = customtkinter.CTkRadioButton(self, text="CTkRadioButton 2", command="", value=2)
+        radiobutton_2.pack()
+
         button = customtkinter.CTkButton(self, fg_color="red", text="Limpar")
         button.pack()
 
